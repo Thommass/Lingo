ceaa5a667cb5e27ea400674a757c20135402ce89
Thomas van Gaalen <thomasvangaalen@icloud.com>
2018-09-28 17:53:25 +0200

    Lingo

---
+++ lingo.js
@@ -0,0 +1,636 @@
+var woordenlijst = [
+    "appel",
+    "aldus",
+    "afwas",
+    "aftel",
+    "aarde",
+    "armen",
+    "actie",
+    "apart",
+    "adres",
+    "avond",
+    "aders",
+    "alarm",
+    "boten",
+    "balen",
+    "beter",
+    "bomen",
+    "boren",
+    "boven",
+    "boxen",
+    "brood",
+    "broek",
+    "brand",
+    "breed",
+    "benen",
+    "beeld",
+    "brief",
+    "beten",
+    "basis",
+    "blauw",
+    "beren",
+    "buren",
+    "banen",
+    "bloed",
+    "broer",
+    "blond",
+    "boter",
+    "beleg",
+    "breng",
+    "baken",
+    "beker",
+    "blind",
+    "bezig",
+    "baden",
+    "bedel",
+    "bazen",
+    "bazin",
+    "baren",
+    "beden",
+    "beken",
+    "bezem",
+    "baard",
+    "bidet",
+    "breuk",
+    "conus",
+    "cello",
+    "creme",
+    "cloud",
+    "cacao",
+    "cadet",
+    "cavia",
+    "ceder",
+    "combi",
+    "china",
+    "clown",
+    "draai",
+    "deden",
+    "dalen",
+    "derde",
+    "delen",
+    "dwaas",
+    "daden",
+    "dader",
+    "dames",
+    "diner",
+    "datum",
+    "dozen",
+    "dreun",
+    "duits",
+    "dagen",
+    "deren",
+    "dwerg",
+    "dwaal",
+    "dwing",
+    "druil",
+    "droog",
+    "draad",
+    "dweil",
+    "drank",
+    "duren",
+    "dwars",
+    "drugs",
+    "daten",
+    "daler",
+    "doorn",
+    "disco",
+    "degen",
+    "droom",
+    "dient",
+    "drone",
+    "dadel",
+    "duwen",
+    "druif",
+    "deken",
+    "deler",
+    "elven",
+    "eigen",
+    "enger",
+    "engel",
+    "elder",
+    "enkel",
+    "effen",
+    "email",
+    "egaal",
+    "Freek",
+    "fiets",
+    "friet",
+    "files",
+    "forel",
+    "films",
+    "feest",
+    "fruit",
+    "falen",
+    "flora",
+    "fauna",
+    "feeen",
+    "freak",
+    "forum",
+    "fusie",
+    "geven",
+    "gaven",
+    "groen",
+    "graai",
+    "getal",
+    "grens",
+    "grond",
+    "groef",
+    "graal",
+    "gewei",
+    "games",
+    "grote",
+    "groet",
+    "garen",
+    "gebak",
+    "graag",
+    "genre",
+    "glans",
+    "geluk",
+    "geeuw",
+    "horen",
+    "heren",
+    "halen",
+    "hagel",
+    "haren",
+    "helen",
+    "harde",
+    "hemel",
+    "hoofd",
+    "huren",
+    "hamer",
+    "haken",
+    "heden",
+    "hotel",
+    "hobby",
+    "heler",
+    "hoger",
+    "ieder",
+    "index",
+    "immer",
+    "icoon",
+    "inlog",
+    "inzet",
+    "innig",
+    "jovel",
+    "jaren",
+    "jicht",
+    "jabot",
+    "jacht",
+    "jaden",
+    "jagen",
+    "jager",
+    "japon",
+    "jarig",
+    "jawel",
+    "jeans",
+    "jemig",
+    "jeugd",
+    "joint",
+    "jonas",
+    "joule",
+    "kappa",
+    "koken",
+    "kreet",
+    "koker",
+    "kerst",
+    "kegel",
+    "koude",
+    "kader",
+    "krent",
+    "kamer",
+    "kaars",
+    "kaart",
+    "kraan",
+    "krant",
+    "keren",
+    "kruid",
+    "kerel",
+    "kubus",
+    "kraal",
+    "kleur",
+    "kroon",
+    "klein",
+    "korst",
+    "klopt",
+    "kabel",
+    "kunst",
+    "kopje",
+    "krans",
+    "klimt",
+    "kater",
+    "klink",
+    "kudde",
+    "kruis",
+    "lopen",
+    "laten",
+    "lepel",
+    "links",
+    "laden",
+    "leven",
+    "lezen",
+    "lucht",
+    "lenen",
+    "laser",
+    "lente",
+    "licht",
+    "lader",
+    "leder",
+    "lunch",
+    "lijst",
+    "leger",
+    "leden",
+    "legen",
+    "lagen",
+    "lezer",
+    "lever",
+    "lingo",
+    "loper",
+    "luier",
+    "lager",
+    "leeuw",
+    "maand",
+    "malen",
+    "maken",
+    "media",
+    "meter",
+    "motor",
+    "maten",
+    "markt",
+    "mazen",
+    "molen",
+    "meest",
+    "meren",
+    "model",
+    "meden",
+    "maden",
+    "macht",
+    "meeuw",
+    "mager",
+    "magen",
+    "maren",
+    "manen",
+    "noord",
+    "nieuw",
+    "negen",
+    "namen",
+    "neven",
+    "nodig",
+    "naden",
+    "neder",
+    "nemen",
+    "onder",
+    "optel",
+    "ovaal",
+    "ovale",
+    "onwel",
+    "optie",
+    "orden",
+    "oppas",
+    "ouder",
+    "ophef",
+    "oases",
+    "palen",
+    "plein",
+    "pegel",
+    "paars",
+    "prijs",
+    "piano",
+    "pixel",
+    "paden",
+    "pasta",
+    "pizza",
+    "poten",
+    "paard",
+    "puber",
+    "pauze",
+    "preek",
+    "polis",
+    "pater",
+    "proef",
+    "panda",
+    "penis",
+    "prins",
+    "pluto",
+    "polen",
+    "plint",
+    "quota",
+    "quant",
+    "quark",
+    "queue",
+    "quilt",
+    "quote",
+    "robot",
+    "reken",
+    "raden",
+    "regen",
+    "radio",
+    "rente",
+    "regio",
+    "rugby",
+    "reden",
+    "roken",
+    "ruzie",
+    "ruist",
+    "regel",
+    "racen",
+    "races",
+    "riool",
+    "ramen",
+    "radar",
+    "roman",
+    "rokje",
+    "razen",
+    "roede",
+    "staan",
+    "staal",
+    "speel",
+    "steeg",
+    "stoel",
+    "stook",
+    "steek",
+    "schep",
+    "spijs",
+    "stoep",
+    "shirt",
+    "samen",
+    "sites",
+    "sport",
+    "spalk",
+    "sjaal",
+    "storm",
+    "staat",
+    "steun",
+    "strak",
+    "serie",
+    "shows",
+    "schat",
+    "snoep",
+    "sfeer",
+    "smeer",
+    "speer",
+    "scene",
+    "speld",
+    "smeed",
+    "smaak",
+    "super",
+    "stand",
+    "steer",
+    "smelt",
+    "sedan",
+    "skier",
+    "sluis",
+    "sneer",
+    "steel",
+    "truck",
+    "terug",
+    "typen",
+    "talen",
+    "taboe",
+    "tegel",
+    "taart",
+    "tafel",
+    "trouw",
+    "teken",
+    "teren",
+    "taken",
+    "treur",
+    "tenen",
+    "titel",
+    "thuis",
+    "tiara",
+    "teder",
+    "toets",
+    "tabak",
+    "trein",
+    "tarwe",
+    "telen",
+    "teler",
+    "uiten",
+    "uilig",
+    "uitje",
+    "uiver",
+    "ultra",
+    "uniek",
+    "uppie",
+    "uraan",
+    "uiers",
+    "velen",
+    "vloer",
+    "video",
+    "varen",
+    "vegen",
+    "veren",
+    "vader",
+    "vaten",
+    "vuren",
+    "vrouw",
+    "vlees",
+    "vogel",
+    "vroeg",
+    "vezel",
+    "veins",
+    "vorst",
+    "veder",
+    "vanaf",
+    "vieze",
+    "veger",
+    "villa",
+    "veler",
+    "vrede",
+    "vries",
+    "woord",
+    "wagen",
+    "wonen",
+    "waren",
+    "warme",
+    "weten",
+    "water",
+    "weren",
+    "wazig",
+    "wegen",
+    "weven",
+    "wezen",
+    "weken",
+    "wraak",
+    "wilde",
+    "wreed",
+    "wrede",
+    "wenst",
+    "woest",
+    "xenon",
+    "yacht",
+    "yucca",
+    "zwaar",
+    "zware",
+    "zesde",
+    "zagen",
+    "zalig",
+    "zomer",
+    "zeden",
+    "zwart",
+    "zeven",
+    "zicht",
+    "zadel",
+    "zweet",
+    "zenuw",
+    "zweer",
+    "zweef",
+    "zaden",
+    "zaken",
+    "zeker",
+    "zever",
+    "zeeen"];
+var woord = woordenlijst[Math.floor(Math.random()*woordenlijst.length)];
+var WoordCopy = woord.split('');
+var poging;
+
+var table = document.getElementsByTagName("table")[0];
+for (var o = 1; o <= 6; o++) {
+    if (o <= 5) {
+        var tr = document.createElement("TR");
+        tr.setAttribute("id", o);
+        for (var i = 1; i <= 5; i++) {
+            var td = document.createElement("TD");
+            var input = document.createElement("INPUT");
+            input.setAttribute("type", "text");
+            input.setAttribute("id", 'rij' + o + 'letter' + i );
+            input.setAttribute("maxlength", "1");
+            td.appendChild(input);
+            tr.appendChild(td);
+        }
+    }
+    table.appendChild(tr);
+}
+
+
+var rij1letter1 = document.getElementById("rij1letter1");
+var rij1letter2 = document.getElementById("rij1letter2");
+var rij1letter3 = document.getElementById("rij1letter3");
+var rij1letter4 = document.getElementById("rij1letter4");
+var rij1letter5 = document.getElementById("rij1letter5");
+//rij2
+var rij2letter1 = document.getElementById("rij2letter1");
+var rij2letter2 = document.getElementById("rij2letter2");
+var rij2letter3 = document.getElementById("rij2letter3");
+var rij2letter4 = document.getElementById("rij2letter4");
+var rij2letter5 = document.getElementById("rij2letter5");
+//rij3
+var rij3letter1 = document.getElementById("rij3letter1");
+var rij3letter2 = document.getElementById("rij3letter2");
+var rij3letter3 = document.getElementById("rij3letter3");
+var rij3letter4 = document.getElementById("rij3letter4");
+var rij3letter5 = document.getElementById("rij3letter5");
+//rij4
+var rij4letter1 = document.getElementById("rij4letter1");
+var rij4letter2 = document.getElementById("rij4letter2");
+var rij4letter3 = document.getElementById("rij4letter3");
+var rij4letter4 = document.getElementById("rij4letter4");
+var rij4letter5 = document.getElementById("rij4letter5");
+//rij5
+var rij5letter1 = document.getElementById("rij5letter1");
+var rij5letter2 = document.getElementById("rij5letter2");
+var rij5letter3 = document.getElementById("rij5letter3");
+var rij5letter4 = document.getElementById("rij5letter4");
+var rij5letter5 = document.getElementById("rij5letter5");
+
+var row1 = [rij1letter1, rij1letter2, rij1letter3, rij1letter4, rij1letter5];
+var row2 = [rij2letter1, rij2letter2, rij2letter3, rij2letter4, rij2letter5];
+var row3 = [rij3letter1, rij3letter2, rij3letter3, rij3letter4, rij3letter5];
+var row4 = [rij4letter1, rij4letter2, rij4letter3, rij4letter4, rij4letter5];
+var row5 = [rij5letter1, rij5letter2, rij5letter3, rij5letter4, rij5letter5];
+var rows = [row1, row2, row3, row4, row5];
+var rownumer = 0;
+
+var eersteletter = woord.charAt(0);
+console.log(woord);
+
+rij1letter1.value = eersteletter;
+
+
+function check() {
+    poging = '';
+    for (var a = 0; a <= 4; a++) {
+        poging += rows[rownumer][a].value;
+    }
+    WoordCopy = woord.split('');
+    for (var g = 0; g <= 4; g++) {
+        if (poging[g] === WoordCopy[g]) {
+            WoordCopy[g] = null;
+            rows[rownumer][g].setAttribute('class', 'goed');
+            rows[rownumer][g].disabled = true
+        }
+    }
+    for(var n = 0; n <= 4; n++){
+        if(WoordCopy.includes(poging[n]) && WoordCopy[WoordCopy.indexOf(poging[n])] !== null){
+            WoordCopy[WoordCopy.indexOf(poging[n])] = null;
+            rows[rownumer][n].setAttribute('class', 'verkeerde');
+            rows[rownumer][n].disabled = true;
+        }
+    }
+
+
+    startNewRow();
+}
+
+function startNewRow() {
+    for (var a = 0; a <= 4; a++) {
+        rows[rownumer][a].disabled = true;
+    }
+    rownumer++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (var y = 0; y <= 4; y++) {
+//     if (WoordCopy.includes(poging[y]) && WoordCopy[WoordCopy.indexOf(poging[y])] !== null) {
+//         WoordCopy[y] = null;
+//         rows[rownumer][y].style.backgroundColor = "yellow";
+//     }
+//
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function checkLetters() {
+//
+//     for(var i = 0; i <= 4; i++) {
+//         if(splitPoging[i] === splitWoordCopy[i]) {
+//             rows[rownumer][i].style.backgroundColor = "green";
+//         } else if (splitWoordCopy.includes(splitPoging[i].toLowerCase())) {
+//             rows[rownumer][i].style.backgroundColor = "orange";
+//         } else {
+//             rows[rownumer][i].style.backgroundColor = "red";
+//         }
+//     }
+//     startNewRow();
+// }
+
+
+
+//rows[rownummer][i]
